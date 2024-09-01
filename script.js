fed0c0a8bd910fe1b9d39d9f1d7d08b7b43792d4
Deva Ahmad <devaahmad43@gmail.com>
2024-09-01 07:37:00 +0700

    Calculator Class

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+class Calculator{
+    constructor(previousOperand, currentOperand){
+        this.previousOperand = previousOperand
+        this.currentOperand = currentOperand
+        this.clear()
+    }
+    clear (){
+        this.previousOperand = ``
+        this.currentOperand = ``
+        this.operation = undefined
+    }
+    delete(){
+
+    }
+    appendNumber(number){
+
+    }
+    operation(operation){
+
+    }
+    calculate(){
+
+    }
+    updateDisplay(){
+
+    }
+}
+
 const numberButtons = document.querySelectorAll(`[dataNumber]`)
 const operationButtons = document.querySelectorAll(`[dataOperand]`)
 const equalButton = document.querySelector('[dataEqual]')
@@ -5,3 +33,4 @@ const deleteButton = document.querySelector(`[dataDelete]`)
 const allClearButton = document.querySelector(`[dataAllClear]`)
 const previousOperand = document.querySelector(`[dataPO]`)
 const currentOperand = document.querySelector(`[dataCO]`)
+
